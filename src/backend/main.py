77b16d07d6161d4204b915abde45d0c2a19452f1
MrForquest <folp228@gmail.com>
2024-08-23 14:05:42 +0300

    fix thread

--- src/backend/main.py
+++ src/backend/main.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 from flask_socketio import SocketIO
 from gevent import monkey
 
-monkey.patch_all(ssl=False, time=False)
+monkey.patch_all(ssl=False)
 
 module_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(module_dir))
