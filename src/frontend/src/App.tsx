817c0ff1ba8e51b75b4030ccb8451f6e0673f255
K372470 <ki2915568@gmail.com>
2024-08-23 13:56:29 +0300

    fix site routes

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { ProgressPanel } from "./components/progress-panel/ProgressPanel";
 import { TopPanel } from "./components/TopPanel";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, redirect } from "react-router-dom";
 import { LineDrawerCanvas } from "./components/pages/line-drawer/LineDrawerCanvas";
 import { FileUploader } from "./components/pages/file-uploader/FileUploader";
 import { startListening } from "./redux/api";
@@ -20,9 +20,10 @@ export const App = () => {
       <TopPanel height="55px" />
       <ProgressPanel />
       <Routes>
-        <Route index path="/docs" element={<FileUploader />} />
+        <Route path="/docs" loader={() => redirect("/")} />
+        <Route index path="/" element={<FileUploader />} />
         <Route path="/edit" element={<LineDrawerCanvas />} />
-        <Route path="/result" element={<ResultPage/>} />
+        <Route path="/result" element={<ResultPage />} />
       </Routes>
     </>
   );
